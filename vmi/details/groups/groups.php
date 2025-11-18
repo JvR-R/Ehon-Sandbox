fdd8a865cb56db38dd116207d603ec7fc45d0aa2
Javier Riveros <javierr@petroindustrial.com.au>
2025-11-18 10:00:56 +1000

    Comment out the webflow_rep.js script in groups.php to prevent loading issues while maintaining DataTables functionality.

--- vmi/details/groups/groups.php
+++ vmi/details/groups/groups.php
@@ -244,7 +244,7 @@
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     
-    <script src="/js/webflow_rep.js" type="text/javascript"></script>
+    <!-- <script src="/js/webflow_rep.js" type="text/javascript"></script> -->
 
     <script>
     $(document).ready(function() {
