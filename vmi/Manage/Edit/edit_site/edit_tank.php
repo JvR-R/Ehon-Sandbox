614d6baa4b7c6bb955b200b9152acacc859a7db1
Javier Riveros <javierr@petroindustrial.com.au>
2025-11-25 14:19:16 +1000

    Remove edit_site.php to streamline the codebase and eliminate unused functionality, ensuring better organization and maintainability.

--- vmi/Manage/Edit/edit_site/edit_tank.php
+++ vmi/Manage/Edit/edit_site/edit_tank.php
@@ -25,10 +25,9 @@ if (isset($_GET['site_id'])) {
     <link rel="stylesheet" href="/vmi/details/menu.css">
     <link href="/vmi/css/style_rep.css" rel="stylesheet" type="text/css">
     <link href="/vmi/css/test-site-de674e.webflow.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="modern_style.css">
+    <link rel="stylesheet" href="/vmi/Manage/Edit/edit_site/modern_style.css">
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
--- vmi/Manage/Edit/edit_site/index.php
+++ vmi/Manage/Edit/edit_site/index.php
@@ -18,10 +18,9 @@ include('../../db/border.php');
     <link rel="stylesheet" href="/vmi/details/menu.css">
     <link href="/vmi/css/style_rep.css" rel="stylesheet" type="text/css">
     <link href="/vmi/css/test-site-de674e.webflow.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="modern_style.css">
+    <link rel="stylesheet" href="/vmi/Manage/Edit/edit_site/modern_style.css">
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="script.js"></script>
 </head>
 <body>
 <div style="opacity:1" class="page-wrapper">
