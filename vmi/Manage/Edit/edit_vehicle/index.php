697fd657d7c6a61024bec02f64a335104ac61a0e
Javier Riveros <javierr@petroindustrial.com.au>
2025-11-25 11:04:49 +1000

    Add additional CSS link for report styling in vehicle edit page to enhance layout consistency

--- vmi/Manage/Edit/edit_vehicle/index.php
+++ vmi/Manage/Edit/edit_vehicle/index.php
@@ -22,6 +22,7 @@ include(BORDER_PATH);
     <link rel="stylesheet" href="/vmi/details/menu.css">
     <link rel="stylesheet" href="../edit_common.css">
     <link rel="stylesheet" href="modern_style.css">
+    <link rel="stylesheet" href="/vmi/css/style_rep.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
