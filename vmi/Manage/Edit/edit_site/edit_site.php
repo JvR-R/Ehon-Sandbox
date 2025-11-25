70e1a27d8edfe59fde2d9e47b4df0c38e1659129
Javier Riveros <javierr@petroindustrial.com.au>
2025-11-25 14:14:52 +1000

    Remove deprecated add_tank.php, add_tank_sbmt.php, edit_site.php, edit_site_sbmt.php, and edit_tank.php files to streamline codebase and eliminate unused functionality.

--- vmi/Manage/Edit/edit_site/edit_site.php
+++ vmi/Manage/Edit/edit_site/edit_site.php
@@ -1,7 +1,7 @@
 <?php
-include('../../db/dbh2.php'); 
-include('../../db/log.php');
-include('../../db/border.php');
+include('../../../db/dbh2.php'); 
+include('../../../db/log.php');
+include('../../../db/border.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,14 +12,13 @@ include('../../db/border.php');
     <meta property="og:type" content="website">
     <meta content="summary_large_image" name="twitter:card">
     <meta content="width=device-width, initial-scale=1" name="viewport">
-    <!-- THEME CSS - MUST BE FIRST -->
     <link rel="stylesheet" href="/vmi/css/theme.css">
-    <!-- Other CSS files -->
     <link href="/vmi/css/normalize.css" rel="stylesheet" type="text/css">
     <link href="/vmi/css/webflow.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/vmi/details/menu.css">
     <link href="/vmi/css/style_rep.css" rel="stylesheet" type="text/css">
     <link href="/vmi/css/test-site-de674e.webflow.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="modern_style.css">
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="script.js"></script>
@@ -31,7 +30,7 @@ include('../../db/border.php');
         <div class="sidebar-spacer"></div>
         <div class="sidebar-spacer2"></div>
             <div class = "dashboard-main-content">
-            <?php include('../../details/top_menu.php');?>
+            <?php include('../../../details/top_menu.php');?>
                 <div class="container-default w-container" style="padding-top: 24px; max-width: 960px;">
                     <!-- Division for input fields -->
                     <div class="mg-bottom-24px">
--- vmi/Manage/Edit/edit_site/edit_tank.php
+++ vmi/Manage/Edit/edit_site/edit_tank.php
@@ -25,6 +25,7 @@ if (isset($_GET['site_id'])) {
     <link rel="stylesheet" href="/vmi/details/menu.css">
     <link href="/vmi/css/style_rep.css" rel="stylesheet" type="text/css">
     <link href="/vmi/css/test-site-de674e.webflow.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="modern_style.css">
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="script.js"></script>
